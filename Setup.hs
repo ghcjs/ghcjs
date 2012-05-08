d79d864324d04f656d35b55ace294e5ccd5a8c77
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2012-05-08 18:43:12 +1200

    Make ghcjs install the files that used to be installed ghcjs-rts back into ghc.
    Since we are now using ghcjs repository to build the the integrated ghc+ghcjs there seems little reason to maintain two copies of the rts files.

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,41 @@
-import Distribution.Simple (defaultMainWithHooks, simpleUserHooks, hookedPrograms)
+import Distribution.Simple (defaultMainWithHooks, simpleUserHooks, hookedPrograms, instHook, copyHook)
 import Distribution.Simple.Program.Types (simpleProgram)
+import Distribution.Simple.Setup (CopyDest(..), installVerbosity, copyVerbosity, fromFlag)
+import Distribution.Simple.Utils (createDirectoryIfMissingVerbose, installOrdinaryFile)
+import Distribution.Simple.LocalBuildInfo (buildDir, absoluteInstallDirs, InstallDirs(..))
+import System.FilePath ((</>))
 
 main = defaultMainWithHooks simpleUserHooks {
     hookedPrograms = [simpleProgram "java"]
+  , instHook = (\pkg_descr lbi hooks flags -> do
+        -- Run the normal install
+        instHook simpleUserHooks pkg_descr lbi hooks flags
+
+        -- Also copy the JavaScript RTS files
+        let lib         = libdir $absoluteInstallDirs pkg_descr lbi NoCopyDest
+            destination = lib </> "rts.jso"
+            verbosity   = fromFlag (installVerbosity flags)
+            copy n      = installOrdinaryFile verbosity ("rts" </> n) (destination </> n)
+
+        createDirectoryIfMissingVerbose verbosity True destination
+        copy "rts-options.js"
+        copy "rts-common.js"
+        copy "rts-plain.js"
+        copy "rts-trampoline.js")
+
+  , copyHook = (\pkg_descr lbi hooks flags -> do
+        -- Run the normal copy
+        copyHook simpleUserHooks pkg_descr lbi hooks flags
+
+        -- Also copy the JavaScript RTS files
+        let lib         = libdir $absoluteInstallDirs pkg_descr lbi NoCopyDest
+            destination = lib </> "rts.jso"
+            verbosity   = fromFlag (copyVerbosity flags)
+            copy n      = installOrdinaryFile verbosity ("rts" </> n) (destination </> n)
+
+        createDirectoryIfMissingVerbose verbosity True destination
+        copy "rts-options.js"
+        copy "rts-common.js"
+        copy "rts-plain.js"
+        copy "rts-trampoline.js")
   }
---
+++ rts/GHCJS.hs
@@ -0,0 +1,19 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  GHCJS
+-- Copyright   :
+-- License     :  BSD3
+--
+-- Maintainer  :  hamish.k.mackenzie@googlemail.com
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module GHCJS (
+
+) where
+
+
