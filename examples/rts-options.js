1c147e94b8e7031734d7af7d62d7dda3ef036cf7
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2012-05-08 14:06:10 +1200

    Add missing rts-options.js file

---
+++ examples/rts-options.js
@@ -0,0 +1,45 @@
+/**
+ * @define {boolean} HS_DEBUG is like goog.DEBUG, but for ghcjs internals
+ */
+var HS_DEBUG = true;
+
+/**
+ * @define {boolean} enable weak pointers and finalizers
+ */
+var HS_WEAKS = true;
+
+/**
+ * @define {boolean} enable traceLog in the run loop
+ */
+var HS_TRACE = true;
+
+/**
+ * @define {boolean} enable tracing in hscall
+ */
+var HS_TRACE_CALLS = false;
+
+/**
+ * @define {boolean} enable include args in the hscall messages
+ */
+var HS_TRACE_ARGS = false;
+
+/**
+ * @define {boolean} enable tracing of RTS calls
+ */
+var HS_RTS_TRACE = true;
+
+/**
+ * @define {boolean} enable tracing of RTS calls
+ */
+var HS_RTS_TRACE = true;
+
+/**
+ * @define {number} enable the trampoline calling convention
+ */
+var HS_TRAMPOLINE = false;
+
+/**
+ * @define {number} size of Word and Int. If 64 we use goog.math.Long.
+ */
+var WORD_SIZE_IN_BITS = 32;
+
