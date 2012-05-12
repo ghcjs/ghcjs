b36362d8381ffad453160e5ed36eeb58a0eace81
Luite Stegeman <stegeman@gmail.com>
2012-05-12 12:26:57 +0200

    remove some code duplication

--- Setup.hs
+++ Setup.hs
@@ -5,37 +5,28 @@ import Distribution.Simple.Utils (createDirectoryIfMissingVerbose, installOrdina
 import Distribution.Simple.LocalBuildInfo (buildDir, absoluteInstallDirs, InstallDirs(..))
 import System.FilePath ((</>))
 
-main = defaultMainWithHooks simpleUserHooks {
-    hookedPrograms = [simpleProgram "java"]
-  , instHook = (\pkg_descr lbi hooks flags -> do
-        -- Run the normal install
-        instHook simpleUserHooks pkg_descr lbi hooks flags
+main = defaultMainWithHooks simpleUserHooks
+         { hookedPrograms = [simpleProgram "java"]
+         , instHook = ghcjsInstHook
+         , copyHook = ghcjsCopyHook
+         }
 
-        -- Also copy the JavaScript RTS files
-        let lib         = libdir $absoluteInstallDirs pkg_descr lbi NoCopyDest
-            destination = lib </> "rts.jso"
-            verbosity   = fromFlag (installVerbosity flags)
-            copy n      = installOrdinaryFile verbosity ("rts" </> n) (destination </> n)
+ghcjsInstHook pkg_descr lbi hooks flags = do
+  instHook simpleUserHooks pkg_descr lbi hooks flags
+  copyRts pkg_descr lbi $ fromFlag (installVerbosity flags)
 
-        createDirectoryIfMissingVerbose verbosity True destination
-        copy "rts-options.js"
-        copy "rts-common.js"
-        copy "rts-plain.js"
-        copy "rts-trampoline.js")
+ghcjsCopyHook pkg_descr lbi hooks flags = do
+  copyHook simpleUserHooks pkg_descr lbi hooks flags
+  copyRts pkg_descr lbi $ fromFlag (copyVerbosity flags)
 
-  , copyHook = (\pkg_descr lbi hooks flags -> do
-        -- Run the normal copy
-        copyHook simpleUserHooks pkg_descr lbi hooks flags
+copyRts pkg_descr lbi verbosity = do
+  let lib         = libdir $absoluteInstallDirs pkg_descr lbi NoCopyDest
+      destination = lib </> "rts.jso"
+      copy n      = installOrdinaryFile verbosity ("rts" </> n) (destination </> n)
 
-        -- Also copy the JavaScript RTS files
-        let lib         = libdir $absoluteInstallDirs pkg_descr lbi NoCopyDest
-            destination = lib </> "rts.jso"
-            verbosity   = fromFlag (copyVerbosity flags)
-            copy n      = installOrdinaryFile verbosity ("rts" </> n) (destination </> n)
+  createDirectoryIfMissingVerbose verbosity True destination
+  copy "rts-options.js"
+  copy "rts-common.js"
+  copy "rts-plain.js"
+  copy "rts-trampoline.js"
 
-        createDirectoryIfMissingVerbose verbosity True destination
-        copy "rts-options.js"
-        copy "rts-common.js"
-        copy "rts-plain.js"
-        copy "rts-trampoline.js")
-  }
