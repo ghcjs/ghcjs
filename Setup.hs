b39beced8b07660113557e1fcb5b0da692bbeace
Luite Stegeman <stegeman@gmail.com>
2012-05-12 13:17:02 +0200

    add support for booting to cabal/Setup

--- Setup.hs
+++ Setup.hs
@@ -1,15 +1,28 @@
-import Distribution.Simple (defaultMainWithHooks, simpleUserHooks, hookedPrograms, instHook, copyHook)
+import Distribution.Simple (defaultMainWithHooks, simpleUserHooks, hookedPrograms, UserHooks(..))
 import Distribution.Simple.Program.Types (simpleProgram)
 import Distribution.Simple.Setup (CopyDest(..), installVerbosity, copyVerbosity, fromFlag)
 import Distribution.Simple.Utils (createDirectoryIfMissingVerbose, installOrdinaryFile)
-import Distribution.Simple.LocalBuildInfo (buildDir, absoluteInstallDirs, InstallDirs(..))
+import Distribution.Simple.LocalBuildInfo (buildDir, absoluteInstallDirs, InstallDirs(..), LocalBuildInfo(..))
+import Distribution.PackageDescription (PackageDescription(..), BuildInfo(..), Executable(..))
 import System.FilePath ((</>))
+import System.Process (rawSystem)
+import Control.Monad (when)
+import Data.Maybe (maybe, listToMaybe)
 
 main = defaultMainWithHooks simpleUserHooks
          { hookedPrograms = [simpleProgram "java"]
-         , instHook = ghcjsInstHook
-         , copyHook = ghcjsCopyHook
+         , instHook       = ghcjsInstHook
+         , postInst       = ghcjsPostInst
+         , copyHook       = ghcjsCopyHook
          }
+
+ghcjsPostInst _ _ pkgDesc lbi = when doBoot (autoboot >> return ())
+  where
+    ghcjsexe = listToMaybe $ filter (\(Executable s _ _) -> s == "ghcjs") 
+                 (executables . localPkgDescr $ lbi)
+    doBoot   = maybe False (any (==("x-boot", "True")).customFieldsBI.buildInfo) ghcjsexe
+
+autoboot = rawSystem "ghcjs-boot" ["--auto"] -- fixme make sure that the one from the current install is being run
 
 ghcjsInstHook pkg_descr lbi hooks flags = do
   instHook simpleUserHooks pkg_descr lbi hooks flags
