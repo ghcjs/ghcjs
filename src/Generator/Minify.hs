40a46bbb587c10623f82d9ff6effdcf2eadeaf10
Luite Stegeman <stegeman@gmail.com>
2012-05-28 19:52:34 +0200

    move RTS dependencies to separate module

--- src/Generator/Minify.hs
+++ src/Generator/Minify.hs
@@ -9,8 +9,10 @@ import System.FilePath ((</>))
 import Data.List (stripPrefix, intercalate)
 import System.Process (system)
 import System.Exit (ExitCode)
+
 import Closure.Paths
-import Paths_ghcjs
+import Compiler.Variants
+import RTS.Dependencies
 
 data MinifyOptions = MinifyOptions {
     hsDebug :: Bool,
@@ -30,50 +32,31 @@ minify jsexe mainFiles options args = do
                         Just n  -> read n
                         Nothing -> 0
 
-    d <- closureLibraryPath
     cj <- closureCompilerJar
-    rts <- getDataFileName "rts"
-    system . intercalate " " $ [
-        "java",
-        "-Xmx1G",
-        "-jar", cj,
-        "--js", d </> "closure/goog/base.js",
-        "--js", d </> "closure/goog/object/object.js",
-        "--js", d </> "closure/goog/string/string.js",
-        "--js", d </> "closure/goog/debug/error.js",
-        "--js", d </> "closure/goog/asserts/asserts.js",
-        "--js", d </> "closure/goog/array/array.js",
-        "--js", d </> "closure/goog/math/long.js",
-        "--js", d </> "closure/goog/math/integer.js",
-        "--js", d </> "closure/goog/debug/relativetimeprovider.js",
-        "--js", d </> "closure/goog/debug/formatter.js",
-        "--js", d </> "closure/goog/structs/structs.js",
-        "--js", d </> "closure/goog/structs/collection.js",
-        "--js", d </> "closure/goog/iter/iter.js",
-        "--js", d </> "closure/goog/structs/map.js",
-        "--js", d </> "closure/goog/structs/set.js",
-        "--js", d </> "closure/goog/useragent/useragent.js",
-        "--js", d </> "closure/goog/debug/debug.js",
-        "--js", d </> "closure/goog/debug/logrecord.js",
-        "--js", d </> "closure/goog/debug/logbuffer.js",
-        "--js", d </> "closure/goog/debug/logger.js",
-        "--js", d </> "closure/goog/debug/console.js",
-        "--js", d </> "closure/goog/crypt/hash.js",
-        "--js", d </> "closure/goog/crypt/md5.js",
-        "--js", rts </> "rts-options.js",
-        define "HS_DEBUG" hsDebug,
-        define "HS_TRACE" hsTrace,
-        define "HS_WEAKS" hsWeaks,
-        "--js", rts </> "rts-common.js",
-        "--js", rts </> "rts-trampoline.js",
-        "--module", "rts:26:"]
-        ++ (map (\x -> "--js " ++ jsexe </> ("hs" ++ show x ++ ".js") ++
-                        " --module hs" ++ show x ++ "min:1:rts") [1..bundleCount])
-        ++ ["--js", jsexe </> "hsloader.js"]
-        ++ (map ("--js "++) mainFiles)
-        ++ ["--module", "main:"++show (length mainFiles + 1)++":rts",
-        "--module_output_path_prefix", jsexe ++ "/",
-        "--compilation_level", "ADVANCED_OPTIMIZATIONS"] ++ args
+    deps <- rtsDeps trampolineVariant
+    system . intercalate " " $
+                [ "java"
+                , "-Xmx1G"
+                , "-jar", cj
+                , define "HS_DEBUG" hsDebug
+                , define "HS_TRACE" hsTrace
+                , define "HS_WEAKS" hsWeaks
+                ] ++ jss deps                                      ++
+                mod "rts" "" (length deps + 1)                     ++
+                bundleMods bundleCount                             ++
+                js (jsexe </> "hsloader.js")                       ++
+                jss mainFiles                                      ++
+                mod "main" "rts" (length mainFiles + 1)            ++
+                [ "--module_output_path_prefix", jsexe ++ "/"
+                , "--compilation_level", "ADVANCED_OPTIMIZATIONS"
+                ] ++ args
   where
-    define s f = "--define='" ++ s ++ if f options then "=true'" else "=false'"
-
+    define s f       = "--define='" ++ s ++ if f options then "=true'" else "=false'"
+    js f             = ["--js", f]
+    jss              = concatMap js
+    mod m d n        = ["--module", intercalate ":" [m, show n, d]]
+    jsMod m d n f    = ["--js", f] ++ mod m d n
+    bundleMods count = concatMap
+                         (\x -> jsMod ("hs"++show x++"min") "rts" 1
+                                (jsexe </> "hs" ++ show x ++ ".js"))
+                         [1..count]
---
+++ src/RTS/Dependencies.hs
@@ -0,0 +1,51 @@
+{-
+  The Javascript dependencies of the runtime system
+-}
+module RTS.Dependencies where
+
+import System.FilePath ((</>))
+
+import Compiler.Variants
+import Closure.Paths
+import Paths_ghcjs
+
+-- | the absolute filepaths of the dependencies for the RTS
+rtsDeps :: Variant -> IO [FilePath]
+rtsDeps v = do
+  rtsPath <- getDataFileName "rts"
+  closurePath <- closureLibraryPath
+  return $ map (rtsPath </>) (rtsSrcs v)
+        ++ map (closurePath </>) closureDeps
+
+rtsSrcs :: Variant -> [FilePath]
+rtsSrcs v =
+    case variantCallingConvention v of
+      Plain      -> [ "rts-common.js", "rts-plain.js" ]
+      Trampoline -> [ "rts-common.js", "rts-trampoline.js" ]
+
+closureDeps :: [FilePath]
+closureDeps =
+   [ "closure/goog/base.js"
+   , "closure/goog/object/object.js"
+   , "closure/goog/string/string.js"
+   , "closure/goog/debug/error.js"
+   , "closure/goog/asserts/asserts.js"
+   , "closure/goog/array/array.js"
+   , "closure/goog/math/long.js"
+   , "closure/goog/math/integer.js"
+   , "closure/goog/debug/relativetimeprovider.js"
+   , "closure/goog/debug/formatter.js"
+   , "closure/goog/structs/structs.js"
+   , "closure/goog/structs/collection.js"
+   , "closure/goog/iter/iter.js"
+   , "closure/goog/structs/map.js"
+   , "closure/goog/structs/set.js"
+   , "closure/goog/useragent/useragent.js"
+   , "closure/goog/debug/debug.js"
+   , "closure/goog/debug/logrecord.js"
+   , "closure/goog/debug/logbuffer.js"
+   , "closure/goog/debug/logger.js"
+   , "closure/goog/debug/console.js"
+   , "closure/goog/crypt/hash.js"
+   , "closure/goog/crypt/md5.js"
+   ]
