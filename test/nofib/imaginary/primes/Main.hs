52719f7ec8b0a3429a7ce5471c2f8f052b64b1e9
Luite Stegeman <stegeman@gmail.com>
2013-03-13 20:08:07 +0100

    import some nofib benchmarks

---
+++ test/nofib/imaginary/primes/Main.hs
@@ -0,0 +1,21 @@
+
+import System.Environment
+import Data.Maybe
+
+suCC :: Int -> Int
+suCC x = x + 1
+
+isdivs :: Int  -> Int -> Bool
+isdivs n x = mod x n /= 0
+
+the_filter :: [Int] -> [Int]
+the_filter (n:ns) = filter (isdivs n) ns
+
+primes :: [Int]
+primes = map head (iterate the_filter (iterate suCC 2))
+
+main = do
+	as <- getArgs
+        let arg = fromMaybe "3000" (listToMaybe as)
+	print $ primes !! (read arg)
+
