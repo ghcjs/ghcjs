6db2ae7d716cd9696e250be774e39e66c494620e
Luite Stegeman <stegeman@gmail.com>
2013-05-20 08:13:17 +0200

    don't print ugly GHC panic messages anymore

--- src-bin/Ghcjs.hs
+++ src-bin/Ghcjs.hs
@@ -2,7 +2,7 @@
 module Main where
 
 import           Paths_ghcjs
-
+import           Data.Typeable
 import qualified GHC.Paths
 import           GHC
 import           HscMain (hscSimplify)
@@ -11,6 +11,7 @@ import           CoreToStg (coreToStg)
 import           SimplStg (stg2stg)
 import           DynFlags
 import           Platform
+import           ErrUtils (fatalErrorMsg'')
 import           CorePrep (corePrepPgm)
 import           DriverPhases (HscSource (HsBootFile), Phase (..),
                                isHaskellSrcFilename, isSourceFilename)
@@ -304,15 +305,37 @@ handleOneShot settings args
 -- | make sure we don't show panic messages with the "report GHC bug" text, since
 --   those are probably our fault.
 errorHandler :: (ExceptionMonad m, MonadIO m)
-             => FatalMessager -> FlushOut -> m a -> m a
-errorHandler fm fo inner = defaultErrorHandler fm fo (convertError inner)
-  where
-    convertError m = handleGhcException (\ge ->
-                       throwGhcException $
-                         case ge of
-                           Panic str -> ProgramError str
-                           x         -> x
-                     ) m
+                    => FatalMessager -> FlushOut -> m a -> m a
+errorHandler fm (FlushOut flushOut) inner =
+  ghandle (\exception -> liftIO $ do
+           flushOut
+           case fromException exception of
+                -- an IO exception probably isn't our fault, so don't panic
+                Just (ioe :: IOException) ->
+                  fatalErrorMsg'' fm (show ioe)
+                _ -> case fromException exception of
+                     Just UserInterrupt -> exitWith (ExitFailure 1)
+                     Just StackOverflow ->
+                         fatalErrorMsg'' fm "stack overflow: use +RTS -K<size> to increase it"
+                     _ -> case fromException exception of
+                          Just (ex :: ExitCode) -> liftIO $ throwIO ex
+                          _ -> case fromException exception of
+                               Just (Panic str) -> fatalErrorMsg'' fm str
+                               _ -> fatalErrorMsg'' fm (show exception)
+           exitWith (ExitFailure 1)
+         ) $
+
+  -- error messages propagated as exceptions
+  handleGhcException
+            (\ge -> liftIO $ do
+                flushOut
+                case ge of
+                     PhaseFailed _ code -> exitWith code
+                     Signal _ -> exitWith (ExitFailure 1)
+                     _ -> do fatalErrorMsg'' fm (show ge)
+                             exitWith (ExitFailure 1)
+            ) $
+  inner
 
 sourceErrorHandler m = handleSourceError (\e -> do
   GHC.printException e
@@ -320,8 +343,11 @@ sourceErrorHandler m = handleSourceError (\e -> do
 
 fatalMessager :: String -> IO ()
 fatalMessager str = do
-  args <- getArgs
-  hPutStrLn stderr (str ++ "\n--- arguments: \n" ++ unwords args ++ "\n---\n")
+  hPutStrLn stderr str
+  dumpArgs <- getEnvOpt "GHCJS_ERROR_ARGUMENTS"
+  when dumpArgs $ do
+    args <- getArgs
+    hPutStrLn stderr (str ++ "\n--- arguments: \n" ++ unwords args ++ "\n---\n")
   exitWith (ExitFailure 1)
 
 {-
