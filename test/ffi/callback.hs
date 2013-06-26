b612661ae4f87904709716479ca2681c29433415
Luite Stegeman <stegeman@gmail.com>
2013-06-26 15:57:51 +0200

    update runSync testcase: with and without async continue

--- test/ffi/callback.hs
+++ test/ffi/callback.hs
@@ -2,6 +2,9 @@
 
 module Main where
 
+import Control.Concurrent
+import Control.Concurrent.MVar
+
 import GHCJS.Foreign
 import GHCJS.Types
 
@@ -12,9 +15,20 @@ runCallback :: JSFun (IO ()) -> IO ()
 runCallback = error "not JavaScript"
 #endif
 
+
+
 main = do
-  s <- syncCallback (putStrLn "b")
+  mv1 <- newEmptyMVar
+  mv2 <- newEmptyMVar
+  s1 <- syncCallback False (putStrLn "b" >> takeMVar mv1 >> putStrLn "error")
+  s2 <- syncCallback True  (putStrLn "d" >> takeMVar mv2 >> putStrLn "f")
   putStrLn "a"
-  runCallback s
+  runCallback s1
   putStrLn "c"
+  runCallback s2
+  putStrLn "e"
+  threadDelay 1000000
+  putMVar mv1 ()
+  putMVar mv2 ()
+  threadDelay 1000000
 
