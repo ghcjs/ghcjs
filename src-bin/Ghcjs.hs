a1132eee26c2fcf116b151b61b353dc85de0c347
Daniil Frumin <difrumin@gmail.com>
2013-08-18 17:15:33 +0400

    Moving 'runGhcSession' to a separate module

--- src-bin/Ghcjs.hs
+++ src-bin/Ghcjs.hs
@@ -16,7 +16,6 @@ import           SimplStg (stg2stg)
 import           UniqFM (eltsUFM)
 import           DynFlags
 import           Platform
-import           ErrUtils (fatalErrorMsg'')
 import           CorePrep (corePrepPgm)
 import           DriverPhases (HscSource, Phase(..),
                                isHaskellSrcFilename, isHaskellUserSrcFilename,
@@ -83,6 +82,8 @@ import           System.FilePath
 import           System.IO
 import           System.Process (rawSystem)
 
+import           GHCJS
+
 import           Compiler.Info
 import           Compiler.Variants
 import           Compiler.GhcjsHooks
@@ -98,7 +99,6 @@ import qualified Gen2.Foreign   as Gen2
 import qualified Gen2.Object    as Object
 
 import           Finder (findImportedModule, cannotFindInterface)
-import Debug.Trace
 
 data GhcjsSettings = GhcjsSettings { gsNativeExecutables :: Bool
                                    , gsNoNative          :: Bool
@@ -171,16 +171,12 @@ main =
          mbMinusB | null minusB_args = Nothing
                   | otherwise = Just . drop 2 . last $ minusB_args
      handleCommandline settings args1 mbMinusB
-     libDir <- getGlobalPackageBase
      when (isNothing mbMinusB) checkIsBooted
      (argsS, _) <- parseStaticFlags (map noLoc args1)
      booting <- getEnvOpt "GHCJS_BOOTING"
      when (not noNative || (booting && any (".c" `isSuffixOf`) args1)) $
        generateNative settings oneshot argsS args1 mbMinusB
-     errorHandler
-        fatalMessager
-        defaultFlushOut
-        $ runGhc (mbMinusB `mplus` Just libDir) $
+     runGhcSession mbMinusB $
        do sdflags0 <- getSessionDynFlags
           let sdflags1 = sdflags0 { verbosity = 1 }
           (dflags0, fileish_args, _) <- parseDynamicFlags sdflags1 $ ignoreUnsupported argsS
@@ -273,54 +269,6 @@ printNumericVersion = do
   if booting then putStrLn getGhcCompilerVersion
              else putStrLn getCompilerVersion
 
--- | make sure we don't show panic messages with the "report GHC bug" text, since
---   those are probably our fault.
-errorHandler :: (ExceptionMonad m, MonadIO m)
-                    => FatalMessager -> FlushOut -> m a -> m a
-errorHandler fm (FlushOut flushOut) inner =
-  ghandle (\exception -> liftIO $ do
-           flushOut
-           case fromException exception of
-                -- an IO exception probably isn't our fault, so don't panic
-                Just (ioe :: IOException) ->
-                  fatalErrorMsg'' fm (show ioe)
-                _ -> case fromException exception of
-                     Just UserInterrupt -> exitWith (ExitFailure 1)
-                     Just StackOverflow ->
-                         fatalErrorMsg'' fm "stack overflow: use +RTS -K<size> to increase it"
-                     _ -> case fromException exception of
-                          Just (ex :: ExitCode) -> liftIO $ throwIO ex
-                          _ -> case fromException exception of
-                               Just (Panic str) -> fatalErrorMsg'' fm str
-                               _ -> fatalErrorMsg'' fm (show exception)
-           exitWith (ExitFailure 1)
-         ) $
-
-  -- error messages propagated as exceptions
-  handleGhcException
-            (\ge -> liftIO $ do
-                flushOut
-                case ge of
-                     PhaseFailed _ code -> exitWith code
-                     Signal _ -> exitWith (ExitFailure 1)
-                     _ -> do fatalErrorMsg'' fm (show ge)
-                             exitWith (ExitFailure 1)
-            ) $
-  inner
-
-sourceErrorHandler m = handleSourceError (\e -> do
-  GHC.printException e
-  liftIO $ exitWith (ExitFailure 1)) m
-
-fatalMessager :: String -> IO ()
-fatalMessager str = do
-  hPutStrLn stderr str
-  dumpArgs <- getEnvOpt "GHCJS_ERROR_ARGUMENTS"
-  when dumpArgs $ do
-    args <- getArgs
-    hPutStrLn stderr (str ++ "\n--- arguments: \n" ++ unwords args ++ "\n---\n")
-  exitWith (ExitFailure 1)
-
 modsumToInfo :: GhcMonad m => ModSummary -> m (Maybe ModuleInfo)
 modsumToInfo ms = getModuleInfo (ms_mod ms)
 
@@ -357,13 +305,6 @@ isSuppressed span _ _
 isSuppressed _ SevOutput txt
   | "Linking " `isPrefixOf` txt = True -- would print our munged name
 isSuppressed _ _ _ = False
-
-runGhcSession mbMinusB a = do
-     libDir <- getGlobalPackageBase
-     errorHandler
-        fatalMessager
-        defaultFlushOut $
-          runGhc (mbMinusB `mplus` Just libDir) $ a
 
 -- also generate native code, compile with regular GHC settings, but make sure
 -- that generated files don't clash with ours
---
+++ src/GHCJS.hs
@@ -0,0 +1,79 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module GHCJS
+    (
+      runGhcSession
+    , sourceErrorHandler
+    , module GHC
+    ) where
+
+import DynFlags
+import GHC
+import Panic (handleGhcException)
+import MonadUtils
+import Exception
+import           ErrUtils (fatalErrorMsg'')
+
+import Control.Monad
+import System.Exit (exitWith, ExitCode(..))
+import System.Environment (getArgs)
+import System.IO (stderr, hPutStrLn)
+
+import Compiler.Info
+import Compiler.Variants
+
+runGhcSession :: Maybe FilePath -> Ghc b -> IO b
+runGhcSession mbMinusB a = do
+    libDir <- getGlobalPackageBase
+    errorHandler
+      fatalMessager
+      defaultFlushOut $
+        runGhc (mbMinusB `mplus` Just libDir) $ a
+
+
+-- | make sure we don't show panic messages with the "report GHC bug" text, since
+--   those are probably our fault.
+errorHandler :: (ExceptionMonad m, MonadIO m)
+                    => FatalMessager -> FlushOut -> m a -> m a
+errorHandler fm (FlushOut flushOut) inner =
+  ghandle (\exception -> liftIO $ do
+           flushOut
+           case fromException exception of
+                -- an IO exception probably isn't our fault, so don't panic
+                Just (ioe :: IOException) ->
+                  fatalErrorMsg'' fm (show ioe)
+                _ -> case fromException exception of
+                     Just UserInterrupt -> exitWith (ExitFailure 1)
+                     Just StackOverflow ->
+                         fatalErrorMsg'' fm "stack overflow: use +RTS -K<size> to increase it"
+                     _ -> case fromException exception of
+                          Just (ex :: ExitCode) -> liftIO $ throwIO ex
+                          _ -> case fromException exception of
+                               Just (Panic str) -> fatalErrorMsg'' fm str
+                               _ -> fatalErrorMsg'' fm (show exception)
+           exitWith (ExitFailure 1)
+         ) $
+
+  -- error messages propagated as exceptions
+  handleGhcException
+            (\ge -> liftIO $ do
+                flushOut
+                case ge of
+                     PhaseFailed _ code -> exitWith code
+                     Signal _ -> exitWith (ExitFailure 1)
+                     _ -> do fatalErrorMsg'' fm (show ge)
+                             exitWith (ExitFailure 1)
+            ) $
+  inner
+
+sourceErrorHandler m = handleSourceError (\e -> do
+  GHC.printException e
+  liftIO $ exitWith (ExitFailure 1)) m
+
+fatalMessager :: String -> IO ()
+fatalMessager str = do
+  hPutStrLn stderr str
+  dumpArgs <- getEnvOpt "GHCJS_ERROR_ARGUMENTS"
+  when dumpArgs $ do
+    args <- getArgs
+    hPutStrLn stderr (str ++ "\n--- arguments: \n" ++ unwords args ++ "\n---\n")
+  exitWith (ExitFailure 1)
