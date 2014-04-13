f6ec0a5e368ff37fcf0135ebc6fe6dc5e62658fd
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2014-04-14 05:43:24 +1200

    Fixes #147

--- include/HsBaseConfig.h
+++ include/HsBaseConfig.h
@@ -1,3 +1,7 @@
+#ifndef ghcjs_HOST_OS
+#include "../../ghcjs-boot/boot/base/include/HsBaseConfig.h"
+#else
+
 /* include/HsBaseConfig.h.  Generated from HsBaseConfig.h.in by configure.  */
 /* include/HsBaseConfig.h.in.  Generated from configure.ac by autoheader.  */
 
@@ -612,3 +616,5 @@
 
 /* Define for large files, on AIX-style hosts. */
 /* #undef _LARGE_FILES */
+
+#endif
