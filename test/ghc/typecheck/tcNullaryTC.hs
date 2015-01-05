ad7f6a0e181b8d68b9a0afefb0abf1225779371a
Luite Stegeman <stegeman@gmail.com>
2015-01-05 01:56:48 +0100

    import new tests from GHC testsuite

---
+++ test/ghc/typecheck/tcNullaryTC.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE NullaryTypeClasses #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Main where
+
+class R where
+  f :: Int -> Int
+  g :: a -> a
+
+instance R where
+  f = (+1)
+  g = id
+
+main = print (g (f 0))
