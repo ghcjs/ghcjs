9e8473318a666f1ddd079e10448c584a2a51568b
Luite Stegeman <stegeman@gmail.com>
2015-10-27 17:37:47 +0000

    test suite fixes and adjustments

--- test/conc/syncBlackhole.hs
+++ test/conc/syncBlackhole.hs
@@ -1,15 +1,22 @@
-{-# LANGUAGE JavaScriptFFI, CPP #-}
+{-# LANGUAGE JavaScriptFFI #-}
 module Main where
 
 import Control.Concurrent
 import Control.Concurrent.MVar
 
-#ifdef __GHCJS__
+import Prelude hiding (putStrLn)
+import qualified Data.JSString as JSS
+
 foreign import javascript unsafe "h$runSync(h$mainZCMainzisync)" startSync :: IO ()
-#else
-startSync :: IO ()
-startSync = return ()
-#endif
+
+{-
+   standard IO is implemented with async callbacks in node.js, use a
+   synchronous alternative to avoid the WouldBlock exception
+ -}
+foreign import javascript unsafe "console.log($1);" js_log :: JSS.JSString -> IO ()
+
+putStrLn :: String -> IO ()
+putStrLn = js_log . JSS.pack
 
 fib :: Int -> Int
 fib 0 = 0
