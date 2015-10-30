ce7b5736b53eb34ef5b913efdc560c7dafaf0b79
Luite Stegeman <stegeman@gmail.com>
2015-10-30 22:25:00 +0000

    update ghcjs-prim and rts deps to support callbacks with a return value

--- lib/ghcjs-prim/GHCJS/Prim/Internal.hs
+++ lib/ghcjs-prim/GHCJS/Prim/Internal.hs
@@ -5,6 +5,9 @@
 module GHCJS.Prim.Internal ( blockedIndefinitelyOnMVar
                            , blockedIndefinitelyOnSTM
                            , wouldBlock
+                           , ignoreException
+                           , setCurrentThreadResultException
+                           , setCurrentThreadResultValue
                            ) where
 
 import           Control.Exception
@@ -21,4 +24,34 @@ blockedIndefinitelyOnMVar = toException BlockedIndefinitelyOnMVar
 blockedIndefinitelyOnSTM :: SomeException
 blockedIndefinitelyOnSTM = toException BlockedIndefinitelyOnSTM
 
+ignoreException :: SomeException -> IO ()
+ignoreException _ = return ()
+
+setCurrentThreadResultException :: SomeException -> IO ()
+setCurrentThreadResultException e
+  | Just WouldBlockException <- fromException e =
+      js_setCurrentThreadResultWouldBlock
+  | Just (JSException v _) <- fromException e =
+      js_setCurrentThreadResultJSException v
+  | otherwise =
+      js_setCurrentThreadResultHaskellException (toJSString (show e))
+
+setCurrentThreadResultValue :: IO JSVal -> IO ()
+setCurrentThreadResultValue x = js_setCurrentThreadResultValue =<< x
+
+foreign import javascript unsafe
+  "h$setCurrentThreadResultWouldBlock();"
+  js_setCurrentThreadResultWouldBlock :: IO ()
+
+foreign import javascript unsafe
+  "h$setCurrentThreadResultJSException($1);"
+  js_setCurrentThreadResultJSException :: JSVal -> IO ()
+
+foreign import javascript unsafe
+  "h$setCurrentThreadResultHaskellException($1);"
+  js_setCurrentThreadResultHaskellException :: JSVal -> IO ()
+
+foreign import javascript unsafe
+  "h$setCurrentThreadResultValue($1);"
+  js_setCurrentThreadResultValue :: JSVal -> IO ()
 
