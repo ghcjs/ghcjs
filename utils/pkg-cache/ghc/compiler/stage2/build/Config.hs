3a133fe7f60531089d9597212a9aed1f0ba613f9
Luite Stegeman <stegeman@gmail.com>
2018-04-29 22:38:34 +0200

    automatically prepare GHC source tree
    this removes the requirement to configure and make the GHC source before
    building GHCJS

---
+++ utils/pkg-cache/ghc/compiler/stage2/build/Config.hs
@@ -0,0 +1,64 @@
+{-# LANGUAGE CPP #-}
+module Config where
+
+#include "ghc_boot_platform.h"
+
+data IntegerLibrary = IntegerGMP
+                    | IntegerSimple
+                    deriving Eq
+
+cBuildPlatformString :: String
+cBuildPlatformString = BuildPlatform_NAME
+cHostPlatformString :: String
+cHostPlatformString = HostPlatform_NAME
+cTargetPlatformString :: String
+cTargetPlatformString = TargetPlatform_NAME
+
+cProjectName          :: String
+cProjectName          = "The Glorious Glasgow Haskell Compilation System"
+cProjectGitCommitId   :: String
+cProjectGitCommitId   = "0156a3d815b784510a980621fdcb9c5b23826f1e"
+cProjectVersion       :: String
+cProjectVersion       = "8.2.2"
+cProjectVersionInt    :: String
+cProjectVersionInt    = "802"
+cProjectPatchLevel    :: String
+cProjectPatchLevel    = "2"
+cProjectPatchLevel1   :: String
+cProjectPatchLevel1   = "2"
+cProjectPatchLevel2   :: String
+cProjectPatchLevel2   = ""
+cBooterVersion        :: String
+cBooterVersion        = "8.2.2"
+cStage                :: String
+cStage                = show (STAGE :: Int)
+cIntegerLibrary       :: String
+cIntegerLibrary       = "integer-gmp"
+cIntegerLibraryType   :: IntegerLibrary
+cIntegerLibraryType   = IntegerGMP
+cSupportsSplitObjs    :: String
+cSupportsSplitObjs    = "YES"
+cGhcWithInterpreter   :: String
+cGhcWithInterpreter   = "YES"
+cGhcWithNativeCodeGen :: String
+cGhcWithNativeCodeGen = "YES"
+cGhcWithSMP           :: String
+cGhcWithSMP           = "YES"
+cGhcRTSWays           :: String
+cGhcRTSWays           = "l debug thr thr_debug thr_l thr_p dyn debug_dyn thr_dyn thr_debug_dyn l_dyn thr_l_dyn"
+cGhcRtsWithLibdw      :: Bool
+cGhcRtsWithLibdw      = False
+cGhcEnableTablesNextToCode :: String
+cGhcEnableTablesNextToCode = "YES"
+cLeadingUnderscore    :: String
+cLeadingUnderscore    = "NO"
+cGHC_UNLIT_PGM        :: String
+cGHC_UNLIT_PGM        = "unlit"
+cGHC_SPLIT_PGM        :: String
+cGHC_SPLIT_PGM        = "ghc-split"
+cLibFFI               :: Bool
+cLibFFI               = False
+cGhcThreaded :: Bool
+cGhcThreaded = True
+cGhcDebugged :: Bool
+cGhcDebugged = False
