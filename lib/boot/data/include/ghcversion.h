f4a625bf9c77731765e0fd5882b0f35f74acaf99
Luite Stegeman <stegeman@gmail.com>
2019-05-06 20:32:25 -0700

    update some boot data files

--- lib/boot/data/include/ghcversion.h
+++ lib/boot/data/include/ghcversion.h
@@ -3,7 +3,7 @@
 
 #define __GLASGOW_HASKELL__ 806
 
-#define __GLASGOW_HASKELL_PATCHLEVEL1__ 1
+#define __GLASGOW_HASKELL_PATCHLEVEL1__ 5 
 
 #define MIN_VERSION_GLASGOW_HASKELL(ma,mi,pl1,pl2) (\
    ((ma)*100+(mi)) <  __GLASGOW_HASKELL__ || \
